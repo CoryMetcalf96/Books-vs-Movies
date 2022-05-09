f9d5c7700c8cca6e752a0adeb07fb64c27ed0c6c
Cory Metcalf <corymetcalf@Corys-Macbook-Pro.local>
2022-05-09 12:27:58 -0400

    Added more movie information to the API pull.

--- js/app.js
+++ js/app.js
@@ -19,6 +19,23 @@ function renderMovieData(event) {
 
     // Pull data from log, using .then to stall.
     $.ajax(MOVIEURL + userInput).then(function (data) {
-        $movieOnlyResults.append(data.Title);
+        // Assign the data from the API to set variables.
+        const moviePoster = `<img src="${data.Poster}"/>`
+        const movieTitle = data.Title;
+        const movieYear = data.Year;
+        const movieRating = data.Rated;
+        const movieRelease = data.Released;
+        const movieGenre = data.Genre;
+        const movieActors = data.Actors;
+        const moviePlot = data.Plot;
+        // Add variables to the DOM
+        $movieOnlyResults.append("\n\n\nTitle: " + movieTitle)
+        $movieOnlyResults.append(". Year: " + movieYear)
+        $movieOnlyResults.append(". Rating: " + movieRating)
+        $movieOnlyResults.append(". Release: " + movieRelease)
+        $movieOnlyResults.append(". Genre: " + movieGenre)
+        $movieOnlyResults.append(". Actors: " + movieActors)
+        $movieOnlyResults.append(". Plot: " + moviePlot)
+        $movieOnlyResults.append(moviePoster)
     })
 }
