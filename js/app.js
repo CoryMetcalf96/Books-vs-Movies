e2cad186a920c22e00b2a9e516b24ddd2e1f2f46
Cory Metcalf <corymetcalf@Corys-Macbook-Pro.local>
2022-05-10 09:36:24 -0400

    Added contact form HTML and CSS

--- js/app.js
+++ js/app.js
@@ -53,3 +53,17 @@ function renderMovieData(event) {
         $movieOnlyResults.append(moviePoster);
     })
 }
+
+function sendEmail() {
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'corymetcalf96@gmail.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
